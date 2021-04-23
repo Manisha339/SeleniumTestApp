508fb81cf1dc9ba407e62490311a2f80569b295b
mallu <mallu@HSC-5YQM6Y2>
2021-04-23 11:47:49 +0530

    AboutUs Navigation

---
+++ AdvanceAutomation/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvanceAutomation
+{
+    /// <summary>
+    /// Summary description for test
+    /// </summary>
+    [TestClass]
+    public class AboutUsPOMTest
+    {
+        [TestMethod]
+        public void TestPOM()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                AboutUsPOM about = new AboutUsPOM(driver);
+                about.Goto();
+                Assert.IsTrue(driver.PageSource.Contains("About Simplilearn"));
+            }
+        }
+    }
+}
