bc7372413be00bc58b6bc0b19a2fc49c61d63970
mallu <mallu@HSC-5YQM6Y2>
2021-04-23 10:39:29 +0530

    Iframe

---
+++ AdvanceAutomation/IFrameTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace AdvanceAutomation
+{
+    [TestClass]
+    public class IFrameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("file://C:/Users/mallu/source/repos/SeleniumTestApp/AdvanceAutomation/iframetest.html");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+                var iframes = driver.FindElementsByTagName("iframe");
+                Assert.IsTrue(iframes.Count == 2);
+
+                driver.SwitchTo().Frame(0);
+                var textFromzero = driver.PageSource.Contains("responsive sites");
+                Assert.IsTrue(textFromzero);
+
+                driver.SwitchTo().DefaultContent();
+
+                driver.SwitchTo().Frame(1);
+                var textFromOne = driver.PageSource.Contains("Online Bootcamp");
+                Assert.IsTrue(textFromOne);
+
+                //Console.Read();
+            }
+        }
+    }
+}
---
+++ AdvanceAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdvanceAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdvanceAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5749f0eb-3c18-46b5-90c1-8144f98c6c5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
